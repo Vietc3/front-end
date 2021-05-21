059c4ecf6dacd5e139b1977dc6e4d9e5db40356d
QuocViet <vietdat657@gmail.com>
2021-05-21 22:54:43 +0700

    Chang Logo

--- components/views/article/Products.tsx
+++ components/views/article/Products.tsx
@@ -30,7 +30,7 @@ const Products = ({
                 marginTop="0.5rem" >
                 Shop on
             </chakra.h1>
-            <Image w="170px" src="/logo.png" h="48px"/>
+            <Image pl="5px" w="170px" src="/logo.png" h="40px"/>
             </Box>
             <Box d="flex" flexDirection={{ base: 'column', lg: 'row' }}  as="section" pl={{ base: '0px', lg: "80px" }}
                 pr={{ base: '0px', lg: "80px" }} >
