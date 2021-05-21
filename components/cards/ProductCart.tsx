a9158ec82948df2ac0fe3d28fd87ba2500a3987f
QuocViet <vietdat657@gmail.com>
2021-05-22 03:59:27 +0700

    Colot

--- components/cards/ProductCart.tsx
+++ components/cards/ProductCart.tsx
@@ -1,47 +1,68 @@
-import React,{useState} from "react";
-import { chakra, Box, Image, Flex, useColorModeValue } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { chakra, Box, Image, Flex, useColorModeValue, HStack } from "@chakra-ui/react";
 import _ from 'lodash';
 type Props = {
-    product: any;
+  product: any;
 }
 
-const ProductCard = ({product}:Props) => {
+type ColorProps = {
+  colors: any;
+}
+const ColorCard = ({ colors }: ColorProps) => {
+console.log(colors);
+
+  return (
+    <HStack spacing="10px">
+      {
+        colors.map((color:string)=>(
+          <Box w="25px" h="25px" borderRadius={30} bg={color}/>
+        ))
+
+      }
+    </HStack>)
+
+}
+
+const ProductCard = ({ product }: Props) => {
 
   const [hover, setHover] = useState(false);
-  const urlImage:any =  Object.values(product.photos.photo_set);
+  const urlImage: any = Object.values(product.photos.photo_set);
   const onClick = () => {
-    window.open(process.env.NEXT_PUBLIC_BASE_URL_SALES_PRODUCT+`/products/${product.id}`, '_blank');
+    window.open(process.env.NEXT_PUBLIC_BASE_URL_SALES_PRODUCT + `/products/${product.id}`, '_blank');
   };
 
-    
+  const colors =  product.searchable.colors.filter((color:string)=>
+    color.charAt(0)==="#"
+  )
+
   return (
     <Flex
       bg={useColorModeValue("#F9FAFB", "gray.600")}
       w="full"
       alignItems="center"
       justifyContent="center"
-      onClick={()=>{onClick()}}
+      onClick={() => { onClick() }}
       cursor="pointer"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       style={{ transform: `scale(${hover ? 1.01 : 1})` }}
-            transition="ease-in 0.2s"
-            h={{ base: "100%", lg: "100%" }} 
-            
+      transition="ease-in 0.2s"
+      h={{ base: "100%", lg: "100%" }}
+
     >
       <Box
-       h={{ base: "100%", lg: "100%" }} 
+        h={{ base: "100%", lg: "100%" }}
         w="100%"
         bg={useColorModeValue("white", "gray.800")}
         shadow="lg"
         p={5}
       >
-      
+
 
         <Image
           h="130px"
           w="auto"
-          
+
           mt={2}
           src={urlImage[0].photos[0]}
           alt={product.name}
@@ -56,27 +77,49 @@ const ProductCard = ({product}:Props) => {
             {product.name}
           </chakra.h1> */}
           <chakra.h4
-            color={useColorModeValue("gray.800", "white")}
+            color={useColorModeValue("black", "white")}
             fontWeight="bold"
             fontSize="md"
             textTransform="uppercase"
             pt={5}
           >
-           {product.name.substr(0, 20)}
-                    {product.name.length > 20 ? '...' : ''}
+            {product.name.substr(0, 20)}
+            {product.name.length > 20 ? '...' : ''}
           </chakra.h4>
         </Box>
         <Flex
           alignItems="center"
           justifyContent="space-between"
+          bg="white"
+          roundedBottom="lg"
           px={4}
           py={2}
-          pt={12}
+          pt={4}
+        >
+          <chakra.h1 color="gray.800" fontWeight="bold" fontSize="xs">
+            {`AVAILABLE COLORS`}
+          </chakra.h1>
+        </Flex>
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          px={4}
+          bg="white"
+          roundedBottom="lg"
+        >
+          <ColorCard colors={colors}/>
+        </Flex>
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          px={4}
+          py={2}
+          pt={3}
           bg="white"
           roundedBottom="lg"
         >
           <chakra.h1 color="black" fontWeight="bold" fontSize="lg">
-           {`$${product.price}`}
+            {`$${product.price}`}
           </chakra.h1>
         </Flex>
       </Box>
