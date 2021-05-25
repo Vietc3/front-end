7af2a9ce898f97066214d253f8319442052253d8
QuocViet <vietdat657@gmail.com>
2021-05-25 11:53:17 +0700

    Update

--- components/views/article/Products.tsx
+++ components/views/article/Products.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Image, Box, chakra } from '@chakra-ui/react';
+import { Image, Box, chakra } from '@chakra-ui/react';
 import ProductCart from '../../cards/ProductCart';
 
 type Props = {
@@ -21,7 +21,7 @@ const Products = ({
 
 
     return (
-        <>{products && <>  <Box as="section" d='flex' pl={{ base: '0px', lg: "80px" }}
+        <>{products ? <><Box as="section" d='flex' pl={{ base: '0px', lg: "80px" }}
             pr={{ base: '0px', lg: "80px" }} >
             <chakra.h1
                 fontWeight="bold"
@@ -30,20 +30,20 @@ const Products = ({
                 marginTop="0.5rem" >
                 Shop on
             </chakra.h1>
-            <Image pl="5px" w="170px" src="/logo.png" h="42px"/>
+            <Image pl="5px" w="170px" src="/logo.png" h="42px" />
             </Box>
-            <Box d="flex" flexDirection={{ base: 'column', lg: 'row' }}  h={{lg: "380px" }} pl={{ base: '0px', lg: "80px" }}
+            <Box d="flex" flexDirection={{ base: 'column', lg: 'row' }} h={{ lg: "380px" }} pl={{ base: '0px', lg: "80px" }}
                 pr={{ base: '0px', lg: "80px" }} >
                 {products?.map((product: any) => {
                     return (
-                        <Box w={{ base: "100%", lg: "25%" }}  h={{ base: "100%", lg: "100%" }}  key={product.name + product.id} p={2} >
+                        <Box w={{ base: "100%", lg: "25%" }} h={{ base: "100%", lg: "100%" }} key={product.name + product.id} p={2} >
                             <ProductCart
                                 product={product}
                             />
                         </Box>
                     );
                 })}
-            </Box></>}
+            </Box></> : null}
 
         </>
     );
