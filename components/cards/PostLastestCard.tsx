258bc4b140e9c1af18c8429b56432b81688c807c
Quoc Viet <quocviet.develop@gmail.com>
2021-06-11 02:26:39 +0700

    Fix

--- components/cards/PostLastestCard.tsx
+++ components/cards/PostLastestCard.tsx
@@ -5,11 +5,11 @@ import useColorTheme from '../../hooks/useColorTheme';
 import Card from './Card';
 import _ from 'lodash';
 import { useRouter } from 'next/router';
-import { BsCalendar, BsPencil } from "react-icons/bs";
+import { BsCalendar, BsPencil } from 'react-icons/bs';
 import moment from 'moment';
-import {getTags} from '../../helpers/commonFuction';
-import { AiFillPlayCircle } from "react-icons/ai";
-import {getUrlImage} from '../../helpers/commonFuction';
+import { getTags } from '../../helpers/commonFuction';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { getUrlImage } from '../../helpers/commonFuction';
 import Image from 'next/image';
 interface Props extends BoxProps {
     post: any;
@@ -23,7 +23,8 @@ interface Props extends BoxProps {
 
 type FlexDirection = 'row' | 'column' | undefined;
 
-const PostLastestCard = ({isNextStory,
+const PostLastestCard = ({
+    isNextStory,
     post,
     column = false,
     imgBoxSize,
@@ -31,26 +32,23 @@ const PostLastestCard = ({isNextStory,
     titleFontSize = '1.4rem',
     ...props
 }: Props) => {
-
-    
     const colors = useColorTheme();
     const flexDirection: FlexDirection = useBreakpointValue({ base: 'row', md: column ? 'row' : 'row' });
 
     const router = useRouter();
 
     const onClick = () => {
-        isNextStory ?  window.location.replace(`https://playitright.tv/articles/${post.id}`) : router.push(`/articles/${post.id}`); 
+        isNextStory
+            ? window.location.replace(`https://playitright.tv/articles/${post.id}`)
+            : router.push(`/articles/${post.id}`);
     };
 
     const [hover, setHover] = useState(false);
     const formatDatePublic = (datePublic: any) => {
-        return moment(datePublic).format("Do MMM YY");
-    }
+        return moment(datePublic).format('Do MMM YY');
+    };
 
-    const tags = getTags(post.tags)
-
-   
-    
+    const tags = getTags(post.tags);
 
     return (
         <Card
@@ -68,66 +66,66 @@ const PostLastestCard = ({isNextStory,
             {...props}
             flexDirection={flexDirection}
         >
-            <Box
-                width={{ base: '70%', lg:  '100%'  }}
-                height={{ base: "150px",md:"180px", lg: '300px' }}
-            >
-            
-            <div
-                style={{
-                position: 'relative'
-                            width: '100%',
-                            height: '100%',
-                            maxHeight: '100%',
-                            maxWidth: '100%',
-                        }}
+            <Box width={{ base: '70%', lg: '100%' }} height={{ base: '150px', md: '180px', lg: '300px' }}>
+                <div
+                    style={{
+                        position: 'relative',
+                        width: '100%',
+                        height: '100%',
+                        maxHeight: '100%',
+                        maxWidth: '100%',
+                    }}
+                >
+                    <Image
+                        style={{ width: '100%', height: '100%' }}
+                        src={getUrlImage(post.hero_desktop.url)}
+                        layout="fill"
+                    />
+                </div>
+
+                {post.youtube_url ? (
+                    <HStack
+                        pl="10px"
+                        justify="left"
+                        pos="absolute"
+                        bottom="20px"
+                        width={{ base: '100%', lg: column ? '100%' : 60 }}
                     >
-           <Image
-                    style={{ width: "100%", height: "100%" }}
-                    src={getUrlImage(post.hero_desktop.url)}
-                    alt={'Photo of ' + post.title}
-                    layout="fill"
-                    objectFit="cover"
-
-                /></div>
-
-                {post.youtube_url ? <HStack pl="10px" justify="left" pos="absolute" bottom="20px" width={{ base: '100%', lg: column ? '100%' : 60 }}>
-                    <Icon mr="10px" as={AiFillPlayCircle} h='50px' w='50px' color="white" />
-                </HStack> : null}
-
+                        <Icon mr="10px" as={AiFillPlayCircle} h="50px" w="50px" color="white" />
+                    </HStack>
+                ) : null}
             </Box>
 
-
-
             <Box w="100%" mt={{ base: 1, md: 2 }} ml={{ base: 5, md: 6 }}>
-                {
-                    tags ? tags.map((tag: any) => (
-                        <Tag key={tag} mr={1} mt={1} size="lg" bgColor="red" borderRadius="full">
-                            <TagLabel color="white" fontSize={{base:"xs",lg:"md"}}>{tag}</TagLabel>
-                        </Tag>
-                    )) : null
-                }
+                {tags
+                    ? tags.map((tag: any) => (
+                          <Tag key={tag} mr={1} mt={1} size="lg" bgColor="red" borderRadius="full">
+                              <TagLabel color="white" fontSize={{ base: 'xs', lg: 'md' }}>
+                                  {tag}
+                              </TagLabel>
+                          </Tag>
+                      ))
+                    : null}
 
-                <Flex textAlign="center" alignItems="center" py={3} display={{ base: "none", lg: "flex" }}>
+                <Flex textAlign="center" alignItems="center" py={3} display={{ base: 'none', lg: 'flex' }}>
                     <Flex>
-                        <Icon  mt={0.5} as={BsCalendar} h={4} w={4} color="black" />
+                        <Icon mt={0.5} as={BsCalendar} h={4} w={4} color="black" />
                         <chakra.h2 mx={3} color="gray" fontWeight="bold" fontSize="md">
                             {formatDatePublic(post.public_date)}
                         </chakra.h2>
                     </Flex>
                     <Flex>
-                        <Icon  mt={1} as={BsPencil} h={4} w={4} color="black" />
+                        <Icon mt={1} as={BsPencil} h={4} w={4} color="black" />
                         <chakra.h2 mx={3} color="gray" fontWeight="bold" fontSize="md">
                             {post.author}
                         </chakra.h2>
                     </Flex>
-
                 </Flex>
                 <Text
                     mt={1}
                     mb={2}
                     display="block"
-                    fontSize={{base:"15",lg:"lg"}}
+                    fontSize={{ base: '15', lg: 'lg' }}
                     lineHeight="normal"
                     fontWeight="bold"
                     href="#"
@@ -136,14 +134,14 @@ const PostLastestCard = ({isNextStory,
                     {_.upperFirst(post.title)}
                 </Text>
 
-                <Flex display={{ base: "flex", lg: "none" }}>
-                    <Icon  mt={0.5} as={BsCalendar} h={5} w={4} color="black" />
+                <Flex display={{ base: 'flex', lg: 'none' }}>
+                    <Icon mt={0.5} as={BsCalendar} h={5} w={4} color="black" />
                     <chakra.h2 mx={3} color="gray" fontWeight="bold" fontSize="md">
                         {formatDatePublic(post.public_date)}
                     </chakra.h2>
                 </Flex>
 
-                <Flex display={{ base: "flex", lg: "none" }}>
+                <Flex display={{ base: 'flex', lg: 'none' }}>
                     <Icon mt={1} as={BsPencil} h={5} w={4} color="black" />
                     <chakra.h2 mx={3} color="gray" fontWeight="bold" fontSize="md">
                         {post.author}
