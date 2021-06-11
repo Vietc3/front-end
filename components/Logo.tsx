ea2556d223110e70e4dacfd87d0c28791d71b64f
Quoc Viet <quocviet.develop@gmail.com>
2021-06-11 14:44:03 +0700

    OKKK

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import { Box, Image } from '@chakra-ui/react';
 const Logo = (props: any) => {
     return (
         <Box {...props} >
-            <Image src="/logoTV.png" fontWeight="bold" />
+            <Image w={{base:'80%',lg:"100%"}} h={{base:"80%"}} src="/logoTV.png" fontWeight="bold" />
         </Box>
     );
 };
