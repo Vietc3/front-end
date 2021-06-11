ead2002b4d4de3ec747feb3754ea53dfbb4b0aa2
Quoc Viet <quocviet.develop@gmail.com>
2021-06-11 12:04:57 +0700

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,72 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import React from 'react';
+
 import { ColorModeScript } from '@chakra-ui/react';
-import HeadCustom  from './headCustom';
-// import NextScriptCustom  from './nextScriptCustom';
-import { compact, flatten } from 'lodash';
-export  class NextScriptCustom extends NextScript {
-  render() {
-    const orgNextScripts = flatten(super.render().props.children);
-
-    const scripts = compact(
-      orgNextScripts.map((child) => {
-        if (child.props.id === '__NEXT_DATA__') {
-          return {
-            props: { ...child.props },
-            content: child.props.dangerouslySetInnerHTML.__html
-          };
-        }
-
-        if (child?.type === 'script') {
-          return {
-            props: { ...child.props },
-            content: ''
-          };
-        }
-
-        return null;
-      })
-    );
-
-    const initialFilterer = props => !props.src || !props.src.includes('chunk');
-    const initialLoadScripts = scripts.filter(({ props }) => initialFilterer(props));
-    const chunkedScripts = scripts.filter(({ props }) => !initialFilterer(props));
-
-    const jsContent = `
-      var chunkedScripts = ${JSON.stringify(chunkedScripts)};
-      setTimeout(() => {
-        chunkedScripts.map((script) => {
-          if (!script || !script.props) return;
-          try {
-            var scriptTag = document.createElement('script');
-  
-            scriptTag.src = script.props.src;
-            scriptTag.async = script.props.async;
-            scriptTag.defer = script.props.defer;
-            
-            if (script.props.id) scriptTag.id = script.props.id;
-            if (script.content) scriptTag.innerHTML = script.content;
-            document.body.appendChild(scriptTag);
-          }
-          catch(err) {
-            console.log(err);
-          }
-        });
-      // 1800ms seems like when PageSpeed Insights stop waiting for more js       
-      }, 1800);
-    `;
-
-    return (
-      <>
-        {initialLoadScripts.map(({ props }) => (
-          <script key={props.id} {...props} src={props.src} />
-        ))}
-
-        <script id="__NEXT_SCRIPT_CUSTOM" defer dangerouslySetInnerHTML={{ __html: jsContent }} />
-      </>
-    );
-  }
-}
 export default class Document extends NextDocument {
     static getInitialProps(ctx: DocumentContext) {
         return NextDocument.getInitialProps(ctx);
@@ -75,13 +9,14 @@ export default class Document extends NextDocument {
     render() {
         return (
             <Html>
-                <HeadCustom>
+                <Head>
                 <link rel="shortcut icon" href="/static/favicon.ico" />
-                </HeadCustom>
+                <script src="https://gist.github.com/bravetheheat/d0eeb93c15d689d769b3194629ce36ab.js"></script>
+                </Head>
                 <body>
                     <ColorModeScript initialColorMode="light" />
                     <Main />
-                    <NextScriptCustom />
+                    <NextScript />
                 </body>
             </Html>
         );
