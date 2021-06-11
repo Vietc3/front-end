67a7c001253ec881bc35e743f8a4fcd86a8f06e1
Quoc Viet <quocviet.develop@gmail.com>
2021-06-11 11:22:51 +0700

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 import { ColorModeScript } from '@chakra-ui/react';
-import HeadCustom  from './headCustom';
 import NextScriptCustom  from './nextScriptCustom';
 export default class Document extends NextDocument {
     static getInitialProps(ctx: DocumentContext) {
