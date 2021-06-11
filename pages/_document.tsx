ee93bfd70438740d9293430167fe961910a6e131
Quoc Viet <quocviet.develop@gmail.com>
2021-06-11 13:00:25 +0700

    OK

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,13 @@ export default class Document extends NextDocument {
         return (
             <Html>
                 <Head>
-                <link rel="shortcut icon" href="/static/favicon.ico" />
+                    <link rel="shortcut icon" href="/static/favicon.ico" />
+                    <link rel="preload" href="https://api.playitright.tv/uploads/Kai_Hero_Small_81a088cef4.jpg" as="image" />
                 </Head>
                 <body>
                     <ColorModeScript initialColorMode="light" />
                     <Main />
-                   <NextScript/>
+                    <NextScript />
                 </body>
             </Html>
         );
