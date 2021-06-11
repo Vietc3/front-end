b05edc5cc2ae55ef99c2cec810152a59b6b5054e
Quoc Viet <quocviet.develop@gmail.com>
2021-06-11 12:21:03 +0700

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-
 import { ColorModeScript } from '@chakra-ui/react';
+import DeferNextScript from './deferNextScript';
 export default class Document extends NextDocument {
     static getInitialProps(ctx: DocumentContext) {
         return NextDocument.getInitialProps(ctx);
@@ -11,12 +11,15 @@ export default class Document extends NextDocument {
             <Html>
                 <Head>
                 <link rel="shortcut icon" href="/static/favicon.ico" />
+                <script dangerouslySetInnerHTML={{ __html: `https://gist.github.com/bravetheheat/d0eeb93c15d689d769b3194629ce36ab.js` }} />
+                {/* <script type="text/javascript" 
                 
+                src="https://gist.github.com/bravetheheat/d0eeb93c15d689d769b3194629ce36ab.js"></script> */}
                 </Head>
                 <body>
                     <ColorModeScript initialColorMode="light" />
                     <Main />
-                    <script src="https://gist.github.com/bravetheheat/d0eeb93c15d689d769b3194629ce36ab.js"></script>
+                   <DeferNextScript/>
                 </body>
             </Html>
         );
